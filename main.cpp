83ffe0bd824cf4689888f78a3adc22ecb938bb38
Jaipikun <maska989@gmail.com>
2022-10-10 21:27:59 +0200

    Defined main.cpp

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+float function(float x){
+    float example_function = sin(x);
+    return example_function;
+}
+
+
+main(){
+    return 0;
+}
